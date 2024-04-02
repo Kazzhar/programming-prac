24de8c6cc180f1ca0ceccfc40324225dd4bc8a44
Karthik Nayak <76555721+Kazzhar@users.noreply.github.com>
2024-04-02 22:17:10 +0530

    question added
    Signed-off-by: Karthik Nayak <76555721+Kazzhar@users.noreply.github.com>

---
+++ April 02 04/isomorphicStrings.cpp
@@ -0,0 +1,3 @@
+//      https://leetcode.com/problems/isomorphic-strings/?envType=daily-question&envId=2024-04-02
+//      DS: unordered_map
+//      Algo: none
---
+++ April 02 04/isomorphicStrings.go
@@ -0,0 +1,4 @@
+//		https://leetcode.com/problems/isomorphic-strings/?envType=daily-question&envId=2024-04-02
+//		Data Structure: maps
+//		ALgorithm: none
+
