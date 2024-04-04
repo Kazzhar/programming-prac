6191ed92312c05a45f58d5db64169339f4d3afc1
Karthik Nayak <76555721+Kazzhar@users.noreply.github.com>
2024-04-04 18:01:58 +0530

    Added cpp code
    Signed-off-by: Karthik Nayak <76555721+Kazzhar@users.noreply.github.com>

--- April-03-24/wordSearch.cpp
+++ April-03-24/wordSearch.cpp
@@ -2,3 +2,47 @@
 //      Data Structure: vectors
 //      Algorithm: backtracking, dfs
 
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+int m, n;
+bool solve(vector<vector<char> >& board, string word, int i, int j, int pos) {
+    if (pos >= word.size())
+        return true;
+    if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != word[pos])
+        return false;
+    char curr = board[i][j];
+    board[i][j] = '\0';
+    if (solve(board, word, i + 1, j, pos + 1) ||
+        solve(board, word, i - 1, j, pos + 1) ||
+        solve(board, word, i, j + 1, pos + 1) ||
+        solve(board, word, i, j - 1, pos + 1))
+        return true;
+    board[i][j] = curr;
+    return false;
+}
+int main() {
+    cin >> m >> n;
+    string word;
+    cin >> word;
+    vector<vector<char> > board(m, vector<char>(n));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (board[i][j] == word[0]) {
+                if (solve(board, word, i, j, 0)) {
+                    cout << "true";
+                    return 0;
+                }
+            }
+        }
+    }
+    cout << "false";
+    return 0;
+}
