97f5584dced948db873857bc3cfddd7cdd0f9b20
kazzhar <kayenn109@gmail.com>
2024-10-08 17:25:36 +0000

    8-10-24, strings + optimised solution
    Signed-off-by: kazzhar <kayenn109@gmail.com>

--- cpp/cpp-min_length_remove_substr/time(n) space(n).cpp
+++ cpp/cpp-min_length_remove_substr/time(n) space(n).cpp
@@ -3,3 +3,16 @@
 //      data structure: vectors, strings
 //      algorithm: none
 
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int minLength(string s) {
+        stack<int> st;
+        for(char curr: s){
+            if(st.empty())st.push(curr);
+            else if((curr=='B'&&st.top()=='A')||(curr=='D'&&st.top()=='C')) st.pop();
+            else st.push(curr);
+        }
+        return st.size();
+    }
+};
